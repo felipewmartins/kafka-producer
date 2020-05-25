34da393b427bb3b152071819c1d963161734f64b
Felipe Martins <felipewmartins@gmail.com>
2020-05-24 23:52:52 -0300

    to be continued...

---
+++ src/main/java/io/felipewmartins/kafkaproducer/producer/MessageProducer.java
@@ -0,0 +1,20 @@
+package io.felipewmartins.kafkaproducer.producer;
+
+import java.util.UUID;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class MessageProducer {
+
+	private final KafkaTemplate<String, String> kafkaTemplate;
+	
+	public void send(@RequestBody String data) {
+		final String randomUUID = UUID.randomUUID().toString();
+	}
+}
